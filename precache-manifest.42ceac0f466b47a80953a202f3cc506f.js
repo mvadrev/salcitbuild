e149b1b89a9cc762c5d6723b5f404a0ed4ec5372
mukund <srinivas.mukund@zensark.com>
2020-03-06 17:22:15 +0530

    first commit

---
+++ precache-manifest.42ceac0f466b47a80953a202f3cc506f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e43723876ad2d1d8342480dc40665a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a865a099d1eaa082d4f1",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8317ab4855e3188c8880",
+    "url": "/static/css/main.698f772c.chunk.css"
+  },
+  {
+    "revision": "a865a099d1eaa082d4f1",
+    "url": "/static/js/2.44fbab50.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.44fbab50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8317ab4855e3188c8880",
+    "url": "/static/js/main.213cb38a.chunk.js"
+  },
+  {
+    "revision": "7520846048905bc1d374",
+    "url": "/static/js/runtime-main.f45b4dba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "21bdd7a39d811def0b2e968fc4b3835a",
+    "url": "/static/media/wind.21bdd7a3.png"
+  }
+]);
